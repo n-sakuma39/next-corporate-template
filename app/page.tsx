64168e9be2d7ed2fcef27efb7e390b869d3438f2
n-sakuma39 <naoto.sakuma39@gmail.com>
2024-07-17 16:54:12 +0900

    3章まで完了

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,74 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import ButtonLink from "@/app/_components/ButtonLink/index";
+import { News } from "@/app/_libs/microcms";
+import NewsList from "./_components/NewsList";
+
+const data: {
+  contents: News[];
+} = {
+  contents: [
+    {
+      id: "1",
+      title: "渋谷にオフィスを移転しました。",
+
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
+    },
+    {
+      id: "2",
+      title: "当社SEOが、業界リーダーTOP30に選出されました。",
+
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
+    },
+    {
+      id: "3",
+      title: "テストの記事です。",
+
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/04/19",
+      createdAt: "2023/04/19",
+    },
+  ],
+};
+
 export default function Home() {
   const name = "世界";
+  const sliceData = data.contents.slice(0, 2);
   return (
-    <section className={styles.top}>
-      <div>
-        <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
-        <p className={styles.description}>
-          私たちは市場をリードしているグローバルテックカンパニーです。
-        </p>
-      </div>
-      <Image
-        src="/img-mv.jpg"
-        alt=""
-        className={styles.bgimg}
-        width={4000}
-        height={1200}
-      />
-    </section>
+    <>
+      <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
+          <p className={styles.description}>
+            私たちは市場をリードしているグローバルテックカンパニーです。
+          </p>
+        </div>
+        <Image
+          src="/img-mv.jpg"
+          alt=""
+          className={styles.bgimg}
+          width={4000}
+          height={1200}
+        />
+      </section>
+
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <NewsList news={sliceData} />
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっと見る</ButtonLink>
+        </div>
+      </section>
+    </>
   );
 }
